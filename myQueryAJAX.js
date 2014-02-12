b4ebe179e6e86b4a0b7d85e5e2ef80d1980a0ed9
Arno <arno@advg.com.br>
2014-02-12 10:04:47 -0200

    load por clique!

--- myQueryAJAX.js
+++ myQueryAJAX.js
@@ -1,8 +1,16 @@
 // Meu AJAX
 
 $(function(){
-	$('body').append("<div id='conteudo'> </div>");
-	$("#conteudo").load("conteudo.txt");
+	$('body').append("<div id='conteudo'> </div><button id='carregar'> carregar </button>");
+
+	$("#carregar").click(function(){
+		$("#conteudo").load("conteudo.txt h2",function(a,b,c){
+			alert(a);//responseTxt	
+			alert(b);//StatusTXT	
+			alert(c);//XMLHtmlRequest				
+		});	
+	});
+
 
 		
 });
